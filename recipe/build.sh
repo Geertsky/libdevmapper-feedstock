02039fe01371b2810a928c433f0e3e384b3d6dad
Geert Geurts <geert@verweggistan.eu>
2026-08-14 13:55:41 +0200

    Increase buid number

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
-autoreconf -fi
 ./configure \
   --prefix="${PREFIX}" \
+  --exec-prefix="${PREFIX}" \
+  --sbindir="${PREFIX}/bin" \
+  --with-default-system-dir="${PREFIX}/etc/" \
+  --with-confdir="${PREFIX}/etc" \
   --enable-pkgconfig
-make -j"${CPU_COUNT}" libdm.install
+make -j"${CPU_COUNT}" V=1
+make -j"${CPU_COUNT}" V=1 install
